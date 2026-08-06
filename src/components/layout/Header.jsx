c58300eb2e2af6ed9f3da8c3229919e21c8c682b
Victor Rosario <vros15@yahoo.com>
2026-08-06 15:06:18 -0400

    feat: add branded header with logo mark and component styling

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,16 @@
 import { NavLink } from "react-router";
-
+import logoMark from "../../assets/branding/megamart-mark-primary.svg";
+import "./Header.css";
 
 const Header = () => (
-  <header>
-    <NavLink to="/">MegaMart</NavLink>
+  <header className="header">
+    <NavLink to="/" className="header-brand">
+      <img src={logoMark} alt="" width="28" height="28" className="header-brand-mark" />
+      <span className="header-brand-word">
+        <span className="header-brand-word-mega">Mega</span>
+        <span className="header-brand-word-mart">Mart</span>
+      </span>
+    </NavLink>
 
     <nav>
       <NavLink to="/" end>
